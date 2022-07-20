40a9c07e5be7a96d40b96e431c26b53b26827c91
Steven Tey <stevensteel97@gmail.com>
2022-07-20 14:27:34 -0500

    Cron with GH actions

--- pages/api/run.ts
+++ pages/api/run.ts
@@ -5,6 +5,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await run();
-  res.status(200).json(response);
+  if (req.method === "POST") {
+    try {
+      const { authorization } = req.headers;
+
+      if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+        const response = await run();
+        res.status(200).json(response);
+      } else {
+        res.status(401).json({ success: false });
+      }
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
 }
