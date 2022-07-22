44a3192f80ba546ef4dd874dc773769ebfc25f78
Steven Tey <stevensteel97@gmail.com>
2022-07-21 21:11:39 -0500

    Update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -5,7 +5,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const data = await getPost(32177639); // test post id
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({ message: "No id found" });
+  }
+  const data = await getPost(parseInt(id)); // test post id
   const response = await processPost(data);
   res.status(200).json(response);
 }
